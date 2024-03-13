72a7d0207ef2a8aefc9a2d910a820edac23eeffd
lum-jashari <lumijashari@hotmail.com>
2024-03-13 12:53:00 +0100

    normal and dynamic binding

--- src/Components/MyComponent.vue
+++ src/Components/MyComponent.vue
@@ -1,17 +1,22 @@
 <script setup>
-const myMessage = "yellow world";
-const myNumber = 100;
+// const amazingImage =
+//   "https://images.unsplash.com/photo-1707327956851-30a531b70cda?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
-function add(x, y) {
-  return x + y;
-}
+// const altText = "just a random picture";
+
+// const imageWidth = 400;
+// const imageHeight = 400;
+
+const imageInfo = {
+  src: "https://images.unsplash.com/photo-1707327956851-30a531b70cda?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  alt: "just a random picture",
+  width: 400,
+  height: 400,
+};
 </script>
 
 <template>
-  <h1>{{ myMessage }}</h1>
-  <h2>{{ myNumber }}</h2>
-  <h3>2 + 2 = {{ 2 + 2 }}</h3>
-  <h1>add two numbers {{ add(2, 10) }}</h1>
+  <img :="imageInfo" />
 </template>
 
 <style lang="scss" scoped></style>
