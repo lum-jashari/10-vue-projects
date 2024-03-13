3baebe4ff6ba2e61017431ab953041a345c2dec0
lum-jashari <lumijashari@hotmail.com>
2024-03-13 16:04:42 +0100

    template refs

--- src/Components/MyComponent.vue
+++ src/Components/MyComponent.vue
@@ -1,7 +1,18 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+
+const thisRef = ref("");
+
+onMounted(() => {
+  console.log(thisRef.value);
+  thisRef.value.focus();
+});
+</script>
 
 <template>
-  <div>test</div>
+  <div>
+    <input type="text" placeholder="enter name" ref="thisRef" />
+  </div>
 </template>
 
 <style scoped></style>
